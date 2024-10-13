29764b8f1a7d5dceca27f8dbb3d80fe45f9cfa7f
ardal <rvidal.gutierrez@outlook.com>
2024-10-12 23:29:23 -0500

    base proyect and custom user

---
+++ general/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class User(AbstractUser):
+    profile_picture = models.ImageField(upload_to='profile_pictures', null=True, blank=True)
+
